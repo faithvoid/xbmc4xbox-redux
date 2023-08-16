faithvoid/xbmc4xbox-redux

commit 7896a62245f42866376230bb26a41001aa0eae81

[556d548] musicdb: 0 items is a perfectly valid result when retrieving a list of items (it's not an error)

xbmc/music/MusicDatabase.cpp
@@ -1413,7 +1413,7 @@ bool CMusicDatabase::GetTop100(const CStdString& strBaseDir, CFileItemList& item
     if (iRowsFound == 0)
     {
       m_pDS->close();
-      return false;
+      return true;
     }
     items.Reserve(iRowsFound);
     while (!m_pDS->eof())
@@ -1458,7 +1458,7 @@ bool CMusicDatabase::GetTop100Albums(VECALBUMS& albums)
     if (iRowsFound == 0)
     {
       m_pDS->close();
-      return false;
+      return true;
     }
     while (!m_pDS->eof())
     {
@@ -1493,7 +1493,7 @@ bool CMusicDatabase::GetTop100AlbumSongs(const CStdString& strBaseDir, CFileItem
     if (iRowsFound == 0)
     {
       m_pDS->close();
-      return false;
+      return true;
     }
 
     // get data from returned rows
@@ -1533,7 +1533,7 @@ bool CMusicDatabase::GetRecentlyPlayedAlbums(VECALBUMS& albums)
     if (iRowsFound == 0)
     {
       m_pDS->close();
-      return false;
+      return true;
     }
     while (!m_pDS->eof())
     {
@@ -1568,7 +1568,7 @@ bool CMusicDatabase::GetRecentlyPlayedAlbumSongs(const CStdString& strBaseDir, C
     if (iRowsFound == 0)
     {
       m_pDS->close();
-      return false;
+      return true;
     }
 
     // get data from returned rows
@@ -1609,7 +1609,7 @@ bool CMusicDatabase::GetRecentlyAddedAlbums(VECALBUMS& albums, unsigned int limi
     if (iRowsFound == 0)
     {
       m_pDS->close();
-      return false;
+      return true;
     }
 
     while (!m_pDS->eof())
@@ -1645,7 +1645,7 @@ bool CMusicDatabase::GetRecentlyAddedAlbumSongs(const CStdString& strBaseDir, CF
     if (iRowsFound == 0)
     {
       m_pDS->close();
-      return false;
+      return true;
     }
 
     // get data from returned rows
@@ -2643,7 +2643,7 @@ bool CMusicDatabase::GetGenresNav(const CStdString& strBaseDir, CFileItemList& i
     if (iRowsFound == 0)
     {
       m_pDS->close();
-      return false;
+      return true;
     }
 
     // get data from returned rows
@@ -2698,7 +2698,7 @@ bool CMusicDatabase::GetYearsNav(const CStdString& strBaseDir, CFileItemList& it
     if (iRowsFound == 0)
     {
       m_pDS->close();
-      return false;
+      return true;
     }
 
     // get data from returned rows
@@ -2837,7 +2837,7 @@ bool CMusicDatabase::GetArtistsByWhere(const CStdString& strBaseDir, const Filte
     if (iRowsFound == 0)
     {
       m_pDS->close();
-      return false;
+      return true;
     }
 
     // store the total value of items as a property
@@ -3036,7 +3036,7 @@ bool CMusicDatabase::GetAlbumsByWhere(const CStdString &baseDir, const Filter &f
     if (iRowsFound <= 0)
     {
       m_pDS->close();
-      return false;
+      return true;
     }
 
     // store the total value of items as a property
@@ -3135,7 +3135,7 @@ bool CMusicDatabase::GetSongsByWhere(const CStdString &baseDir, const Filter &fi
     if (iRowsFound == 0)
     {
       m_pDS->close();
-      return false;
+      return true;
     }
 
     // store the total value of items as a property

commit 328ec59cc550b0f512d2b6ef1cb5990cd66c00c7

fixed: fetching songs was broken after 7896a62245f42866376230bb26a41001aa0eae81 - we had infinite loop

xbmc/music/MusicDatabase.cpp
@@ -3217,15 +3217,17 @@ bool CMusicDatabase::GetSongsNav(const CStdString& strBaseDir, CFileItemList& it
   if (idAlbum == -1 && idArtist == -1 && idGenre == -1)
   {
     int iLIMIT = 5000;    // chunk size
+    int itemsSize = 0;
     for (int i=0;;i+=iLIMIT)
     {
       musicUrl.AddOption("limit", iLIMIT);
       musicUrl.AddOption("offset", i);
       Filter filter;
-      if (!GetSongsByWhere(musicUrl.ToString(), filter, items, sortDescription))
-        return items.Size() > 0;
+      GetSongsByWhere(musicUrl.ToString(), filter, items, sortDescription);
+      if (itemsSize == items.Size())
+        return true;
+      itemsSize = items.Size();
     }
-    return true;
   }
 #endif
   if (idArtist > 0)